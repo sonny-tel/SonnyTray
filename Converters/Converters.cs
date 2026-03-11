fc5d6a2d519a40c79e4f23e4107a2d4192746030
Will <39478251+VITALISED@users.noreply.github.com>
2026-03-12 09:46:06 +1100

    Icon

--- Converters/Converters.cs
+++ Converters/Converters.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace SonnyTray.Converters;
 
@@ -66,6 +67,28 @@ public class BackendStateToTextConverter : IValueConverter
             "NoState" => "Logged Out",
             _ => state
         } : "Unknown";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
+
+public class StringToImageSourceConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string url && !string.IsNullOrEmpty(url))
+        {
+            try
+            {
+                return new BitmapImage(new Uri(url, UriKind.Absolute));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        return null;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
