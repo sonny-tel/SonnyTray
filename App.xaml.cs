d546eece26559492bceca380a02ecf3bfd1ff4f1
Will <39478251+VITALISED@users.noreply.github.com>
2026-03-18 14:17:47 +1100

    Squish the T

--- App.xaml.cs
+++ App.xaml.cs
@@ -39,19 +39,19 @@ public partial class App : Application
 
     private static readonly (int X, int Y)[] DotCenters =
     [
-        (6, 6),  (6, 16),  (6, 26),
-        (16, 6), (16, 16), (16, 26),
-        (26, 6), (26, 16), (26, 26),
+        (6, 6),  (16, 6),  (26, 6),
+        (6, 16), (16, 16), (26, 16),
+        (6, 26), (16, 26), (26, 26),
     ];
 
     private static readonly int[] AllDots = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    private static readonly int[] TDots = [0, 3, 4, 5, 6];
+    private static readonly int[] TDots = [3, 4, 5, 7];
 
     private static readonly int[][] AnimFrames =
     [
         [4],
         [3, 4, 5],
-        [0, 3, 4, 5, 6],
+        [3, 4, 5, 7],
         [],
     ];
 
